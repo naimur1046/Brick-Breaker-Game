f0637b69e7bca04d6e136f254b91fee92c12dc54
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2022-08-16 02:04:32 -0700

    The Initial Point of Game
    This is the Initial Point of game

---
+++ code/StartTheGame.java
@@ -0,0 +1,9 @@
+package game1;
+
+public class StartTheGame
+{
+    public static void main(String[] args) {
+        Breakout gamestart=new Breakout();
+        gamestart.setVisible(true);
+    }
+}
