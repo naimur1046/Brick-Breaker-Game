536d6371a8b407f47db07ad439e82c3532ea4690
naimur rahman rumel <88675051+naimur1046@users.noreply.github.com>
2022-08-17 06:59:04 -0700

    This is Common interface.
    This interface is created for keeping every value of the game classes same.

---
+++ code/Common.java
@@ -0,0 +1,21 @@
+package game1;
+
+
+import java.awt.event.KeyEvent;
+
+public interface Commons {
+
+    int WIDTH = 300;
+    int HEIGHT = 400;
+    int BOTTOM_EDGE = 390;
+    int N_OF_BRICKS = 30;
+    int INIT_PADDLE_X = 200;
+    int INIT_PADDLE_Y = 360;
+    int INIT_BALL_X = 230;
+    int INIT_BALL_Y = 355;
+    int PERIOD = 1;
+
+    public void keyPressed(KeyEvent e);
+    public void keyReleased(KeyEvent e);
+    public void doGameCycle();
+}
